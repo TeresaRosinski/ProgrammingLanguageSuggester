c0b1d67b907279a29a37b80e0584925ceba3e8cd
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-06-05 15:00:28 -0500

    It will add a framework for the branching, but it has errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 $(document).ready(function() {
-  $("#formOne").submit(function(event){
+  $("form#formOne").submit(function(event){
     const name = $("input#name").val();
     const company= $("#company").val();
     const system = $("input:radio[name=computer]:checked").val();
     const problemSolvingPref = $("input:radio[name=problem-solving]:checked").val();
     const dataProcessing = $("input:radio[name=data-processing]:checked").val();
     const whichHouse = $("#house").val();
-      //console.log (name);
-      console.log (company);
-      console.log(system);
-      console.log (problemSolvingPref);
-      console.log(dataProcessing);
-      console.log(whichHouse);
+      
+
+    if (dataProcessing === "yesData" && problemSolvingPref === "one-way" && whichHouse === "Slytherin" && company === "Google" || "Spotify") {
+      $("#typeOfCode").text("You should definitely learn Python if you enjoy Data Analytics, want to work for Spotify or Google and you're a Slytherin.");
+      $("#output").show();
+    } else if (problemSolvingPref === "ambiguity" && company === "Hulu" || "Kickstarter") {
+      $("#typeOfCode").text("you could really enjoy Ruby. It allows room for many ways to solve problems and companies such as Hulu and Kickstarter use it.");
+      $("#output").show();
+    } else if (system ===  "apple") {
+      $("#typeOfCode").text("Swift was created by Apple, since you prefer Apple to Microsoft you might enjoy this language.");
+      $("#output").show();
+    }else if (system === "microsoft"){
+      $("#typeOfCode").text("C# was created by Microsoft - you might enjoy learning this language");
+      $("#output").show(); 
+    }
 
 
     event.preventDefault();
