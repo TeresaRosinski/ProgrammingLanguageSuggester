6e620b2afb4627057769c02fe974f4266bb2a0fc
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-06-05 13:54:33 -0500

    It will add all the variables in js

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event){
+    const name = $("input#name").val();
+    const company= $("#company").val();
+    const system = $("input:radio[name=computer]:checked").val();
+    const problemSolvingPref = $("input:radio[name=problem-solving]:checked").val();
+    const dataProcessing = $("input:radio[name=data-processing]:checked").val();
+    const whichHouse = $("#house").val();
+
+
+    event.preventDefault();
+  });
+});
