f11c85e54e8e7fec73fcd126f92d8e5cd9b3cf06
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-06-05 15:33:46 -0500

    It will fix branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,34 @@
 $(document).ready(function() {
   $("form#formOne").submit(function(event){
     const name = $("input#name").val();
-    const company= $("#company").val();
+    const company= $("select#company").val();
     const system = $("input:radio[name=computer]:checked").val();
     const problemSolvingPref = $("input:radio[name=problem-solving]:checked").val();
     const dataProcessing = $("input:radio[name=data-processing]:checked").val();
-    const whichHouse = $("#house").val();
+    const whichHouse = $("select#house").val();
+      console.log(name);
+      console.log(company); 
+      console.log(system);
+      console.log(problemSolvingPref);
+      console.log(dataProcessing);
+      console.log(whichHouse);
       
 
-    if (dataProcessing === "yesData" && problemSolvingPref === "one-way" && whichHouse === "Slytherin" && company === "Google" || "Spotify") {
-      $("#typeOfCode").text("You should definitely learn Python if you enjoy Data Analytics, want to work for Spotify or Google and you're a Slytherin.");
+    if (dataProcessing === "yesData" && problemSolvingPref === "one-way" ){
       $("#output").show();
-    } else if (problemSolvingPref === "ambiguity" && company === "Hulu" || "Kickstarter") {
-      $("#typeOfCode").text("you could really enjoy Ruby. It allows room for many ways to solve problems and companies such as Hulu and Kickstarter use it.");
+      $("#typeOfCode").text("Python");
+    } else if (company === "Hulu" || company === "Kickstarter" && problemSolvingPref === "ambiguity") {
+      $("#typeOfCode").text("Ruby");
       $("#output").show();
     } else if (system ===  "apple") {
-      $("#typeOfCode").text("Swift was created by Apple, since you prefer Apple to Microsoft you might enjoy this language.");
+      $("#typeOfCode").text("Swift");
       $("#output").show();
     }else if (system === "microsoft"){
-      $("#typeOfCode").text("C# was created by Microsoft - you might enjoy learning this language");
+      $("#typeOfCode").text("C#");
       $("#output").show(); 
+    } else {
+      $("#typeOfCode").text("You're a dynamic person, good luck!")
+      $("output").show();
     }
 
 
